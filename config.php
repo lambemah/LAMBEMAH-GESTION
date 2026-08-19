da6600d21ecbac58af9a3fceae8cae205f11fc42
lambemah <konatelambetenin@gmail.com>
2026-08-19 15:57:07 +0000

    Add database connection configuration in config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
+
+if ($conn->connect_error) {
+    die("Erreur de connexion à la base de données : " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
